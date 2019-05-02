477d8ec44fcf79576665cfa47cc12fc07cd93beb
Flemming Krog Olsen <fko@sampension.dk>
2019-05-02 15:52:03 +0200

    Added Storybook build

---
+++ storybook-static/main.105358ea4dba968ad7d7.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(402),p(479),n.exports=p(881)},479:function(n,o,p){"use strict";p.r(o);p(480),p(707),p(710),p(865),p(869)}},[[0,1,2]]]);
